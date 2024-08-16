ff9580b7ff4dd6de0b256ee3a8106e233402b387
Khaled Nached <khalednached@gmail.com>
2024-08-16 16:49:54 -0400

    Fixed a typo

--- resources/views/landing/about.blade.php
+++ resources/views/landing/about.blade.php
@@ -1,140 +1,148 @@
-<x-travelcomponents.header/>
-<x-travelcomponents.navbar/>
-   
-    <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url({{asset('assets/images/tropicalVacay.jpg')}});" data-stellar-background-ratio="0.5">
-      <div class="overlay"></div>
-      <div class="container">
+<x-travelcomponents.header />
+<x-travelcomponents.navbar />
+
+<section class="hero-wrap hero-wrap-2 js-fullheight"
+    style="background-image: url({{ asset('assets/images/tropicalVacay.jpg') }});" data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
         <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-center">
-          <div class="col-md-9 ftco-animate pb-5 text-center">
-            <h1 class="mb-3 bread" style="font-weight:900;">About Us</h1>
-            <p style="font-weight: bold;">What makes us the best travel agency</p>
-          </div>
+            <div class="col-md-9 ftco-animate pb-5 text-center">
+                <h1 class="mb-3 bread" style="font-weight:900;">About Us</h1>
+                <p style="font-weight: bold;">What makes us the best travel agency</p>
+            </div>
         </div>
-      </div>
-    </section>
+    </div>
+</section>
 
-    <section class="ftco-section services-section bg-light">
-      <div class="container">
+<section class="ftco-section services-section bg-light">
+    <div class="container">
         <div class="row d-flex">
-          <div class="col-md-6 order-md-last heading-section pl-md-5 ftco-animate">
-          	<h2 class="mb-4">Start your adventure with us!</h2>
-            <p>Vive Aventuras Caribeñas was founded with the vision of making travel accessible to everyone. </p>
-           <p> We specialize in curating pre-packaged travel bundles that offer the best value for your money. 
-             Whether you're looking for a relaxing beach getaway, a cultural exploration, or an adventure-packed holiday,
-         we have something for every type of traveler.
-        </p>
-        
-            {{-- <p><a href="#" class="btn btn-primary py-3 px-4">Search Destination</a></p> --}}
-          </div>
-          <div class="col-md-6">
-          	<div class="row">
-          		<div class="col-md-6 d-flex align-self-stretch ftco-animate">
-		            <div class="media block-6 services d-block">
-		              <div class="icon"><span class="flaticon-paragliding"></span></div>
-		              <div class="media-body">
-		                <h3 class="heading mb-3">Activities</h3>
-		                <p>A small river named Duden flows by their place and supplies it with the necessary</p>
-		              </div>
-		            </div>      
-		          </div>
-		          <div class="col-md-6 d-flex align-self-stretch ftco-animate">
-		            <div class="media block-6 services d-block">
-		              <div class="icon"><span class="flaticon-route"></span></div>
-		              <div class="media-body">
-		                <h3 class="heading mb-3">Travel Arrangements</h3>
-		                <p>A small river named Duden flows by their place and supplies it with the necessary</p>
-		              </div>
-		            </div>    
-		          </div>
-		          <div class="col-md-6 d-flex align-self-stretch ftco-animate">
-		            <div class="media block-6 services d-block">
-		              <div class="icon"><span class="flaticon-tour-guide"></span></div>
-		              <div class="media-body">
-		                <h3 class="heading mb-3">Private Guide</h3>
-		                <p>A small river named Duden flows by their place and supplies it with the necessary</p>
-		              </div>
-		            </div>      
-		          </div>
-		          <div class="col-md-6 d-flex align-self-stretch ftco-animate">
-		            <div class="media block-6 services d-block">
-		              <div class="icon"><span class="flaticon-map"></span></div>
-		              <div class="media-body">
-		                <h3 class="heading mb-3">Location Manager</h3>
-		                <p>A small river named Duden flows by their place and supplies it with the necessary</p>
-		              </div>
-		            </div>      
-		          </div>
-          	</div>
-          </div>
+            <div class="col-md-6 order-md-last heading-section pl-md-5 ftco-animate">
+                <h2 class="mb-4">Start your adventure with us!</h2>
+                <p>Vive Aventuras Caribeñas was founded with the vision of making travel accessible to everyone. </p>
+                <p> We specialize in curating pre-packaged travel bundles that offer the best value for your money.
+                    Whether you're looking for a relaxing beach getaway, a cultural exploration, or an adventure-packed
+                    holiday,
+                    we have something for every type of traveler.
+                </p>
+
+                {{-- <p><a href="#" class="btn btn-primary py-3 px-4">Search Destination</a></p> --}}
+            </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-6 d-flex align-self-stretch ftco-animate">
+                        <div class="media block-6 services d-block">
+                            <div class="icon"><span class="flaticon-paragliding"></span></div>
+                            <div class="media-body">
+                                <h3 class="heading mb-3">Activities</h3>
+                                <p>A small river named Duden flows by their place and supplies it with the necessary</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 d-flex align-self-stretch ftco-animate">
+                        <div class="media block-6 services d-block">
+                            <div class="icon"><span class="flaticon-route"></span></div>
+                            <div class="media-body">
+                                <h3 class="heading mb-3">Travel Arrangements</h3>
+                                <p>A small river named Duden flows by their place and supplies it with the necessary</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 d-flex align-self-stretch ftco-animate">
+                        <div class="media block-6 services d-block">
+                            <div class="icon"><span class="flaticon-tour-guide"></span></div>
+                            <div class="media-body">
+                                <h3 class="heading mb-3">Private Guide</h3>
+                                <p>A small river named Duden flows by their place and supplies it with the necessary</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 d-flex align-self-stretch ftco-animate">
+                        <div class="media block-6 services d-block">
+                            <div class="icon"><span class="flaticon-map"></span></div>
+                            <div class="media-body">
+                                <h3 class="heading mb-3">Location Manager</h3>
+                                <p>A small river named Duden flows by their place and supplies it with the necessary</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </section>
+    </div>
+</section>
 
-    <section class="ftco-counter img" id="section-counter">
-    	<div class="container mb-5">
-    		<div class="row d-flex">
-    			<div class="col-md-6 d-flex">
-    				<div class="img d-flex align-self-stretch" style="background-image:url({{asset('assets/images/worldTour.jpg')}});"></div>
-    			</div>
-    			<div class="col-md-6 pl-md-5 py-5">
-    				<div class="row justify-content-start pb-3">
-		          <div class="col-md-12 heading-section ftco-animate">
-		            <h2 class="mb-4">Why Choose Us?</h2>
-		          <ul class="list-group list-group-flush">
-                  <li class="list-group-item">
-                  <b>Pre-Packaged Trips:</b> We offer a wide range of pre-packaged travel bundles that include flights, accommodations, and guided tours.
-                  </li>
-                  
-                  <li class="list-group-item">
-                  <b>Customizable Packages:</b>
-                  Want something a little more tailored? We can customize our packages to suit your specific needs and preferences.
-                  </li>
+<section class="ftco-counter img" id="section-counter">
+    <div class="container mb-5">
+        <div class="row d-flex">
+            <div class="col-md-6 d-flex">
+                <div class="img d-flex align-self-stretch"
+                    style="background-image:url({{ asset('assets/images/worldTour.jpg') }});"></div>
+            </div>
+            <div class="col-md-6 pl-md-5 py-5">
+                <div class="row justify-content-start pb-3">
+                    <div class="col-md-12 heading-section ftco-animate">
+                        <h2 class="mb-4">Why Choose Us?</h2>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                                <b>Pre-Packaged Trips:</b> We offer a wide range of pre-packaged travel bundles that
+                                include flights, accommodations, and guided tours.
+                            </li>
 
-                  <li class="list-group-item">
-                  <b>Affordable Pricing:</b>
-                  Our bundles are competitively priced, ensuring you get the best deal for your dream vacation.
-                  </li>
+                            <li class="list-group-item">
+                                <b>Customizable Packages:</b>
+                                Want something a little more tailored? We can customize our packages to suit your
+                                specific needs and preferences.
+                            </li>
 
-                  <li class="list-group-item">
-                  <b>Destination Expertise: </b>
-                  O With our deep knowledge of the Caribbean and other popular destinations, we provide expert guidance and recommendations.
-                  </li>
+                            <li class="list-group-item">
+                                <b>Affordable Pricing:</b>
+                                Our bundles are competitively priced, ensuring you get the best deal for your dream
+                                vacation.
+                            </li>
 
-                  </ul>
-		          </div>
-		        </div>
-		    		<div class="row">
-		          <div class="col-md-4 justify-content-center counter-wrap ftco-animate">
-		            <div class="block-18 text-center mb-4">
-		              <div class="text">
-		                <strong class="number" data-number="300">0</strong>
-		                <span>Successful Tours</span>
-		              </div>
-		            </div>
-		          </div>
-		          <div class="col-md-4 justify-content-center counter-wrap ftco-animate">
-		            <div class="block-18 text-center mb-4">
-		              <div class="text">
-		                <strong class="number" data-number="24000">0</strong>
-		                <span>Happy Tourist</span>
-		              </div>
-		            </div>
-		          </div>
-		          <div class="col-md-4 justify-content-center counter-wrap ftco-animate">
-		            <div class="block-18 text-center mb-4">
-		              <div class="text">
-		                <strong class="number" data-number="200">0</strong>
-		                <span>Place Explored</span>
-		              </div>
-		            </div>
-		          </div>
-		        </div>
-	        </div>
+                            <li class="list-group-item">
+                                <b>Destination Expertise: </b>
+                                With our deep knowledge of the Caribbean and other popular destinations, we provide
+                                expert
+                                guidance and recommendations.
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4 justify-content-center counter-wrap ftco-animate">
+                        <div class="block-18 text-center mb-4">
+                            <div class="text">
+                                <strong class="number" data-number="300">0</strong>
+                                <span>Successful Tours</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 justify-content-center counter-wrap ftco-animate">
+                        <div class="block-18 text-center mb-4">
+                            <div class="text">
+                                <strong class="number" data-number="24000">0</strong>
+                                <span>Happy Tourist</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 justify-content-center counter-wrap ftco-animate">
+                        <div class="block-18 text-center mb-4">
+                            <div class="text">
+                                <strong class="number" data-number="200">0</strong>
+                                <span>Place Explored</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    	</div>
-    </section>
+    </div>
+</section>
 
-    {{-- <section class="ftco-section testimony-section bg-bottom" style="background-image: url(images/bg_3.jpg);">
+{{-- <section class="ftco-section testimony-section bg-bottom" style="background-image: url(images/bg_3.jpg);">
       <div class="container">
         <div class="row justify-content-center pb-4">
           <div class="col-md-7 text-center heading-section ftco-animate">
@@ -220,4 +228,4 @@
       </div>
     </section> --}}
 
-<x-travelcomponents.footer/>
+<x-travelcomponents.footer />
