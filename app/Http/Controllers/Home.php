0bae017a8c882f68c70cf1f2c6f6aaacf6ea630c
Khaled Nached <khalednached@gmail.com>
2024-11-22 16:47:59 -0500

    Fixed page paths

--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -33,11 +33,11 @@ class Home extends Controller
     
    
     public function termsAndConditions(){
-        return view('terms');
+        return view('landing.terms');
     }
 
     public function userPrivacy(){
-        return view('privacy');
+        return view('landing.privacy');
     }
 
     // Optimized for performance 
