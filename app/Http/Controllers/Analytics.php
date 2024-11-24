b918dc0792a3134ae1e2209ba7c35c08b717adfd
knached99 <khalednached@gmail.com>
2024-11-24 17:50:34 -0500

    Removing redundant data

--- app/Http/Controllers/Analytics.php
+++ app/Http/Controllers/Analytics.php
@@ -20,172 +20,78 @@ class Analytics extends Controller
     }
 
 
-   public static function quickAnalytics(){
-    // Static method call in the Admin dashboard page, retrieves 
-    // most visited urls and total number of visits 
-
-    $visitors = VisitorModel::select('visited_url', 'visitor_user_agent', 'visited_at')->get()->toArray();
-    // Return the values from a single column in the mostVisitedURLs array
-    $mostVisitedURLs = array_column($visitors, 'visited_url');
+    public function showAnalytics()
+    {
+        $visitors = VisitorModel::all();
     
+        // Unique IPs
+        $ips = $visitors->pluck('visitor_ip_address')->unique();
     
-    // Counts all the values of an array
-
-    $urlCounts = array_count_values($mostVisitedURLs);
-
-    // Sort an array in reverse order and maintain index association
-
-    arsort($urlCounts);
-    //Gets the first key of an array
-    // Gets the most visited URL (first element in the sorted array)
-    $mostVisitedURL = array_key_first($urlCounts);
-
-    $totalVisitors = count($visitors);
-
-    return [
-        'most_visited_url' => $mostVisitedURL,
-        'total_visitors_count' => $totalVisitors
-    ];
-
-   }
-   
-
-   public function showAnalytics()
-   {
-       $visitors = VisitorModel::all();
-   
-       // Unique IPs
-       $ips = $visitors->pluck('visitor_ip_address')->unique();
-   
-       $locations = [];
-       $browsers = [];
-       $operatingSystems = [];
-   
-       // Get location data based on IPs with caching
-       foreach ($ips as $ip) {
-           // Check cache first before making API call
-           $locations[$ip] = Cache::remember("geo_" . substr(md5($ip), 0, 32), 1440, function() use ($ip) {
-
-               return $this->geoJSService->getLocation($ip);
-           });
-       }
-   
-       // Attach city, state, and country to each visitor
-       foreach ($visitors as $visitor) {
-           $location = $locations[$visitor->visitor_ip_address] ?? null;
-           $visitor->city = $location['city'] ?? null;
-           $visitor->state = $location['region'] ?? null;
-           $visitor->country = $location['country'] ?? null;
-           // Parse user agent to get browser and OS
-           $parsedAgent = $this->parseUserAgent($visitor->visitor_user_agent);
-           $visitor->browser = $parsedAgent['browser'];
-           $visitor->operating_system = $parsedAgent['os'];
-       }
-   
-       // Group visitors by date for the 'Unique Visitors' chart
-       $visitorData = VisitorModel::selectRaw('DATE(visited_at) as visit_date, COUNT(*) as visit_count')
-           ->groupBy('visit_date')
-           ->orderBy('visit_date')
-           ->get();
-   
-       // Aggregate device data
-       $ipData = VisitorModel::selectRaw('visitor_ip_address, COUNT(*) as ip_count')
-           ->groupBy('visitor_ip_address')
-           ->get();
-   
-       // User Agents data
-       $userAgentsData = VisitorModel::selectRaw('visitor_user_agent, COUNT(*) as agent_count')
-           ->groupBy('visitor_user_agent')
-           ->orderBy('agent_count', 'DESC')
-           ->take(5)
-           ->get();
-   
-       // URLs and referrers data
-       $urlData = VisitorModel::selectRaw('visited_url, visitor_ip_address, visitor_user_agent, created_at, COUNT(*) as visit_count, visitor_referrer')
-           ->groupBy('visited_url', 'visitor_ip_address', 'visitor_user_agent', 'visitor_referrer', 'created_at')
-           ->orderBy('visit_count', 'DESC')
-           ->get()
-           ->map(function ($url) use ($locations) {
-               // Use cached location data for IP lookup
-               $location = $locations[Crypt::decryptString($url->visitor_ip_address)] ?? null;
-               $url->geoLocation = $location;
-               $url->city = $location['city'] ?? 'Unknown City';
-               $url->state = $location['region'] ?? 'Unknown State';
-               $url->country = $location['country'] ?? 'Unknown Country';
-               $url->latitude = $location['latitude'] ?? null;
-               $url->longitude = $location['longitude'] ?? null;
-   
-               $parsedAgent = $this->parseUserAgent($url->visitor_user_agent);
-               $url->browser = $parsedAgent['browser'] ?? 'Unknown Browser';
-               $url->operating_system = $parsedAgent['os'] ?? 'Unknown OS';
-   
-               return $url;
-           });
-   
-       foreach ($urlData as $url) {
-           // Update browser count
-           if (isset($browsers[$url->browser])) {
-               $browsers[$url->browser] += $url->visit_count;
-           } else {
-               $browsers[$url->browser] = $url->visit_count;
-           }
-   
-           // Update operating system count, sanitized for valid key
-           $os = isset($url->operating_system) ? preg_replace('/[^a-zA-Z0-9\s]/', '', $url->operating_system) : 'Unknown';
-           if (isset($operatingSystems[$os])) {
-               $operatingSystems[$os] += $url->visit_count;
-           } else {
-               $operatingSystems[$os] = $url->visit_count;
-           }
-       }
-   
-       $topBrowsers = collect($browsers)->sortDesc();
-       $topOperatingSystems = collect($operatingSystems)->sortDesc();
-   
-       // Prepare heatmap data
+        $locations = [];
+        $browsers = [];
+        $operatingSystems = [];
+    
+        // Get location data based on IPs with caching
+        foreach ($ips as $ip) {
+            // Check cache first before making API call
+            $locations[$ip] = Cache::remember("geo_" . substr(md5($ip), 0, 32), 1440, function() use ($ip) {
+                return $this->geoJSService->getLocation($ip);
+            });
+        }
+    
+        // Attach city, state, and country to each visitor
+        foreach ($visitors as $visitor) {
+            $location = $locations[$visitor->visitor_ip_address] ?? null;
+            $visitor->city = $location['city'] ?? null;
+            $visitor->state = $location['region'] ?? null;
+            $visitor->country = $location['country'] ?? null;
+            // Parse user agent to get browser and OS
+            $parsedAgent = $this->parseUserAgent($visitor->visitor_user_agent);
+            $visitor->browser = $parsedAgent['browser'];
+            $visitor->operating_system = $parsedAgent['os'];
+        }
+    
+        // Aggregate device data (browsers and OS)
+        foreach ($visitors as $url) {
+            // Update browser count
+            if (isset($browsers[$url->browser])) {
+                $browsers[$url->browser] += 1;
+            } else {
+                $browsers[$url->browser] = 1;
+            }
+    
+            // Update operating system count, sanitized for valid key
+            $os = isset($url->operating_system) ? preg_replace('/[^a-zA-Z0-9\s]/', '', $url->operating_system) : 'Unknown';
+            if (isset($operatingSystems[$os])) {
+                $operatingSystems[$os] += 1;
+            } else {
+                $operatingSystems[$os] = 1;
+            }
+        }
+    
+        $topBrowsers = collect($browsers)->sortDesc()->take(5); // Limit to top 5 browsers
+        $topOperatingSystems = collect($operatingSystems)->sortDesc()->take(5); // Limit to top 5 OS
+    
+        // Prepare heatmap data
         $heatmapData = $visitors->filter(function ($visitor) {
             return !empty($visitor->country);
         })->groupBy(function ($visitor) {
             return ucfirst(strtolower(trim($visitor->country))); // Normalize country name
         })->map(function ($group) {
             return [
-                'country' => $group->first()->country, 
+                'country' => $group->first()->country,
                 'count' => $group->count()
             ];
         })->values()->toArray();
     
-
-    // $heatmapData = [
-    //     ['country' => 'United States', 'count' => 120],
-    //     ['country' => 'Canada', 'count' => 80],
-    //     ['country' => 'Brazil', 'count' => 50],
-    //     ['country' => 'Germany', 'count' => 40],
-    //     ['country' => 'India', 'count' => 35],
-    //     ['country' => 'United Kingdom', 'count' => 30],
-    //     ['country' => 'France', 'count' => 25],
-    //     ['country' => 'Australia', 'count' => 20],
-    //     ['country' => 'Japan', 'count' => 15],
-    //     ['country' => 'Mexico', 'count' => 10]
-    // ];
-
-
-    // Debug: Log heatmap data
-    \Log::debug('Heatmap Data: ', $heatmapData);
-
-    if (empty($heatmapData)) {
-        \Log::debug('No valid heatmap data found.');
+        // Return only the necessary data
+        return view('admin.analytics', compact(
+            'topBrowsers',
+            'topOperatingSystems',
+            'heatmapData'
+        ));
     }
-   
-       return view('admin.analytics', compact(
-           'visitorData',
-           'ipData',
-           'userAgentsData',
-           'urlData',
-           'heatmapData',
-           'topBrowsers',
-           'topOperatingSystems',
-       ));
-   }
+    
    
 
     /**
--- resources/views/admin/analytics.blade.php
+++ resources/views/admin/analytics.blade.php
@@ -1,119 +1,27 @@
 <x-authenticated-theme-layout>
     <div class="row">
-        <!-- Visits and URLs Table -->
-        <div class="col-lg-12 mb-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Visits and URL Details</h5>
-                    <p class="card-subtitle mb-4">Detailed table showing URLs visited, the number of visits, referrers,
-                        IP addresses, devices, browsers, and countries.</p>
-                    <table class="table dataTable">
-                        <thead>
-                            <tr>
-                                <th>URL</th>
-                                <th>Number of Visits</th>
-                                <th>Referrers</th>
-                                <th>IP Address</th>
-                                <th>Device</th>
-                                <th>Browser</th>
-                                <th>Region/State</th>
-                                <th>City</th>
-                                <th>Country</th>
-                                <th>Date Visited</th>
-                                <th>Map Location</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($urlData as $url)
-                                <tr>
-                                    <td><a rel="noopener noreferrer" target="_blank"
-                                            href="{{ config('app.url') . ':8000' . $url->visited_url }}">{{ $url->visited_url }}</a>
-                                    </td>
-                                    <td>{{ $url->visit_count }}</td>
-                                    <td><a href={{ $url->visitor_referrer ?? '#' }} rel="noopener noreferrer"
-                                            target="_blank">{{ $url->visitor_referrer }}</a>
-                                    </td>
-                                    <td>{{ Crypt::decryptString($url->visitor_ip_address) ?? 'N/A' }}</td>
-                                    <td>{{ $url->operating_system ?? 'N/A' }}</td>
-                                    <td>{{ $url->browser ?? 'N/A' }}</td>
-                                    <td>{{ $url->state ?? 'N/A' }}</td>
-                                    <td>{{ $url->city ?? 'N/A' }}</td>
-                                    <td>{{ $url->country ?? 'N/A' }}</td>
-                                    <td>{{ date('F jS, Y, \a\t g:i A', strtotime($url->created_at)) }}</td>
-                                    <td>
-                                        @if (empty($url->latitude) || empty($url->longitude))
-                                            <span>Map Unavailable</span>
-                                        @else
-                                            <a href="https://www.openstreetmap.org/?mlat={{ $url->latitude }}&mlon={{ $url->longitude }}#map=17/"
-                                                target="_blank" rel="noreferrer noopener">View Location</a>
-                                    </td>
-                            @endif
 
-
-
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-
+        <!-- Visitor Devices Chart -->
         <div class="col-lg-12 mb-6">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Visitor Devices</h5>
-                    <p class="card-subtitle mb-4">This chart shows the devices used by your users to access your site.
-                        This gives you insight to knowing whether or not most of your users come from a mobile
-                        experience or desktop experience</p>
+                    <p class="card-subtitle mb-4">This chart shows the devices used by your users to access your site. It gives insight into whether most of your users come from mobile or desktop experiences.</p>
                     <x-admincomponents.user-agent-pie-chart :topBrowsers="$topBrowsers" :topOperatingSystems="$topOperatingSystems" />
                 </div>
             </div>
         </div>
 
-        <!-- Visitor World Heatmap Chart --> 
-
+        <!-- Visitor World Heatmap Chart -->
         <div class="col-lg-12 mb-6">
-        <div class="card">
-        <div class="card-body">
-        <h5 class="card-title">World Heatmap</h5>
-        <p class="card-subtitle mb-4">
-        This map shows where your visitors come from, with darker areas showing a higher distribution of visitors.</p>
-        </p>
-        <x-admincomponents.heatmap-chart :heatmapData="$heatmapData" /> 
-        </div>
-        </div>
-        </div>
-        <!-- / Visitor World Heatmap Chart -->
-
-
-
-        <!-- Visitor Heatmap Table -->
-        {{-- <div class="col-lg-12 mb-6">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Visitor Density by Country</h5>
-                    <p class="card-subtitle mb-4">Table showing visitor density across different countries.</p>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Country</th>
-                                <th>Visitor Count</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($heatmapData as $data)
-                                <tr>
-                                    <td>{{ $data->country }}</td>
-                                    <td>{{ $data->count }}</td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                    <h5 class="card-title">World Heatmap</h5>
+                    <p class="card-subtitle mb-4">This map shows where your visitors come from, with darker areas indicating higher distribution of visitors.</p>
+                    <x-admincomponents.heatmap-chart :heatmapData="$heatmapData" />
                 </div>
             </div>
-        </div> --}}
-        <!-- /Visitor Heatmap Table -->
+        </div>
 
     </div>
 </x-authenticated-theme-layout>
