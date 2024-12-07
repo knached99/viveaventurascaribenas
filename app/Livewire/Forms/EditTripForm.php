9740cca7573c16ab679bd7bbf7d96794a1c08947
knached99 <khalednached@gmail.com>
2024-12-07 17:07:21 -0500

    Temp workaround to fix email queueing issue

--- app/Livewire/Forms/EditTripForm.php
+++ app/Livewire/Forms/EditTripForm.php
@@ -443,14 +443,13 @@ class EditTripForm extends Component
                         \Log::info('Trip status changed to '.$this->tripAvailability);
                         \Log::info('Fetching all reservations associated with this trip...');
                         $reservations = Reservations::where('tripID', $this->trip->tripID)->get();
-                        \Log::info(['Reservations retrieved: ', json_encode($reservations)]);
+                        \Log::info('Reservations retrieved: ', count($reservations));
                         // Notify all users who made reservations for this trip
                         foreach ($reservations as $reservation) {
                             \Log::info('Notification being sent to: ' . $reservation->email);
                             
                             Notification::route('mail', $reservation->email)
                                 ->notify(new TripAvailableNotification($this->trip, $reservation->reservationID, $reservation->customerName));
-                                \Log::info('Email sent to '.$reservation->customerName);
                             }
                     }
                     
