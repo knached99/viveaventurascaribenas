1215316e884e0a615a946fa5aa2225bc72469e4f
knached99 <khalednached@gmail.com>
2025-01-22 15:39:39 -0500

    Fixing a stupid bug

--- resources/views/landing/destination.blade.php
+++ resources/views/landing/destination.blade.php
@@ -373,8 +373,7 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-       <livewire:testimonial-form :tripID="$trip->tripID" />
-
+       <livewire:forms.testimonial-form :tripID="$trip->tripID" />
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
