d59a7b70a267622e90000e79d3ef433334682e3e
knached99 <khalednached@gmail.com>
2025-02-01 22:14:41 -0500

    Reverting back to pie chart

--- resources/views/components/admincomponents/user-agent-pie-chart.blade.php
+++ resources/views/components/admincomponents/user-agent-pie-chart.blade.php
@@ -1,4 +1,4 @@
-{{-- @props(['topBrowsers', 'topOperatingSystems'])
+@props(['topBrowsers', 'topOperatingSystems'])
 
 <div id="userAgentPieChart"></div>
 
@@ -42,65 +42,6 @@
         };
 
         const chart = new ApexCharts(document.querySelector("#userAgentPieChart"), options);
-        chart.render();
-    });
-</script> --}}
-
-<!-- Bar Chart for User Agents --> 
-
-@props(['topBrowsers', 'topOperatingSystems'])
-
-<div id="userAgentBarChart"></div>
-
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        const topBrowsers = @json($topBrowsers);
-        const topOperatingSystems = @json($topOperatingSystems);
-
-        console.log("Top Browsers:", topBrowsers);
-        console.log("Top Operating Systems:", topOperatingSystems);
-
-        const browserLabels = Object.keys(topBrowsers);
-        const browserSeries = Object.values(topBrowsers);
-        const osLabels = Object.keys(topOperatingSystems);
-        const osSeries = Object.values(topOperatingSystems);
-
-        const options = {
-            chart: {
-                type: 'bar',
-                height: 350,
-            },
-            series: [
-                {
-                    name: 'Browsers',
-                    data: browserSeries
-                },
-                {
-                    name: 'Operating Systems',
-                    data: osSeries
-                }
-            ],
-            xaxis: {
-                categories: browserLabels.length > osLabels.length ? browserLabels : osLabels
-            },
-            title: {
-                text: 'Top Browsers and Operating Systems',
-                align: 'center'
-            },
-            responsive: [{
-                breakpoint: 480,
-                options: {
-                    chart: {
-                        width: 250
-                    },
-                    legend: {
-                        position: 'bottom'
-                    }
-                }
-            }]
-        };
-
-        const chart = new ApexCharts(document.querySelector("#userAgentBarChart"), options);
         chart.render();
     });
 </script>
