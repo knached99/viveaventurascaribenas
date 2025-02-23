991cb9982ffa168ea5ec59cffd339af1b8fb27fd
knached99 <khalednached@gmail.com>
2025-02-23 01:57:27 -0500

    Using Full-Text Search

--- app/Livewire/Forms/Search.php
+++ app/Livewire/Forms/Search.php
@@ -23,89 +23,142 @@ class Search extends Component
     public string $searchQuery = '';
 
     
-    public function search()
-    {
+    public function search(){
+
         $this->validate();
-    
+
         try {
-            // Trips Search
-            $tripsResults = TripsModel::where('tripID', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('tripLocation', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('tripDescription', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('tripLandscape', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('tripAvailability', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('tripStartDate', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('tripEndDate', 'LIKE', "%{$this->searchQuery}%")
-                ->select('tripID', 'tripLocation', 'tripPhoto', 'tripDescription', 'tripLandscape', 'tripAvailability', 'tripStartDate', 'tripEndDate', 'tripPhoto')
-                ->get()
-                ->map(function ($trip) {
-                    $trip['type'] = 'trip';
-                    return $trip;
-                })
-                ->toArray();
-    
-            // Testimonials Search
-            $testimonialsResults = Testimonials::with(['trip'])->where('name', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('email', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('testimonial', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('testimonial_approval_status', 'LIKE', "%{$this->searchQuery}%")
-                ->select('testimonialID', 'tripID', 'name', 'email', 'testimonial')
-                ->get()
-                ->map(function ($testimonial) {
-                    $testimonial['type'] = 'testimonial';
-                    return $testimonial;
-                })
-                ->toArray();
-    
-            // Bookings Search
-            $bookingResults = BookingModel::with(['trip'])->where('bookingID', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('name', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('email', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('phone_number', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('address_line_1', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('address_line_2', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('city', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('state', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('zip_code', 'LIKE', "%{$this->searchQuery}%")
-                ->select('bookingID', 'tripID', 'name', 'email', 'phone_number', 'stripe_product_id')
-                ->get()
-                ->map(function ($booking) {
-                    $booking['type'] = 'booking';
-                    return $booking;
-                })
-                ->toArray();
-    
-            // Reservations Search
-            $reservationsResults = Reservations::with(['trip'])->where('reservationID', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('name', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('email', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('phone_number', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('address_line_1', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('address_line_2', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('city', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('state', 'LIKE', "%{$this->searchQuery}%")
-                ->orWhere('zip_code', 'LIKE', "%{$this->searchQuery}%")
-                ->select('reservationID', 'tripID', 'name', 'email', 'phone_number', 'stripe_product_id')
-                ->get()
-                ->map(function ($reservation) {
-                    $reservation['type'] = 'reservation';
-                    return $reservation;
-                })
-                ->toArray();
-    
+            
+            // Performing full-text search using Scout 
+
+            $tripsResults = TripsModel::search($this->searchQuery)->get();
+            $testimonialsResults = Testimonials::search($this->searchQuery)->get();
+            $bookingResults = BookingModel::search($this->searchQuery)->get();
+            $reservationResults = Reservations::search($this->searchQuery)->get();
+
+            // mapping results to include the type attribute 
+
+            $tripsResults = $tripsResults->map(function($trip){
+                $trip['type'] = 'trip';
+                return $trip;
+            })->toArray();
+
+            $testimonialsResults = $testimonialsResults->map(function($testimonial){
+             $testimonial['type'] = 'testimonial';
+             
+             return $testimonial;
+            })->toArray();
+
+            $bookingResults = $bookingResults->map(function($booking){
+
+                $booking['type'] = 'booking';
+            })->toArray();
+
+            $reservationsResults = $reservationResults->map(function($reservation){
+
+                $reservation['type'] = 'reservation';
+            })->toArray();
+
             $this->searchResults = array_merge($tripsResults, $testimonialsResults, $bookingResults, $reservationsResults);
-    
-            // Suggest similar term if no results found
-            if (empty($this->searchResults)) {
+
+
+            // suggest similar term if no results are found 
+
+            if(empty($this->searchResults)){
                 $this->suggestion = $this->findSimilarTerm($this->searchQuery);
             }
-    
-        } catch (\Exception $e) {
+        }
+
+        catch(\Exception $e){
             $this->searchResults = [];
             $this->searchError = 'Unable to perform search';
             \Log::error("Search Error: {$e->getMessage()}");
+
         }
     }
+    // public function search()
+    // {
+    //     $this->validate();
+    
+    //     try {
+    //         // Trips Search
+    //         $tripsResults = TripsModel::where('tripID', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('tripLocation', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('tripDescription', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('tripLandscape', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('tripAvailability', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('tripStartDate', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('tripEndDate', 'LIKE', "%{$this->searchQuery}%")
+    //             ->select('tripID', 'tripLocation', 'tripPhoto', 'tripDescription', 'tripLandscape', 'tripAvailability', 'tripStartDate', 'tripEndDate', 'tripPhoto')
+    //             ->get()
+    //             ->map(function ($trip) {
+    //                 $trip['type'] = 'trip';
+    //                 return $trip;
+    //             })
+    //             ->toArray();
+    
+    //         // Testimonials Search
+    //         $testimonialsResults = Testimonials::with(['trip'])->where('name', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('email', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('testimonial', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('testimonial_approval_status', 'LIKE', "%{$this->searchQuery}%")
+    //             ->select('testimonialID', 'tripID', 'name', 'email', 'testimonial')
+    //             ->get()
+    //             ->map(function ($testimonial) {
+    //                 $testimonial['type'] = 'testimonial';
+    //                 return $testimonial;
+    //             })
+    //             ->toArray();
+    
+    //         // Bookings Search
+    //         $bookingResults = BookingModel::with(['trip'])->where('bookingID', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('name', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('email', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('phone_number', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('address_line_1', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('address_line_2', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('city', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('state', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('zip_code', 'LIKE', "%{$this->searchQuery}%")
+    //             ->select('bookingID', 'tripID', 'name', 'email', 'phone_number', 'stripe_product_id')
+    //             ->get()
+    //             ->map(function ($booking) {
+    //                 $booking['type'] = 'booking';
+    //                 return $booking;
+    //             })
+    //             ->toArray();
+    
+    //         // Reservations Search
+    //         $reservationsResults = Reservations::with(['trip'])->where('reservationID', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('name', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('email', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('phone_number', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('address_line_1', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('address_line_2', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('city', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('state', 'LIKE', "%{$this->searchQuery}%")
+    //             ->orWhere('zip_code', 'LIKE', "%{$this->searchQuery}%")
+    //             ->select('reservationID', 'tripID', 'name', 'email', 'phone_number', 'stripe_product_id')
+    //             ->get()
+    //             ->map(function ($reservation) {
+    //                 $reservation['type'] = 'reservation';
+    //                 return $reservation;
+    //             })
+    //             ->toArray();
+    
+    //         $this->searchResults = array_merge($tripsResults, $testimonialsResults, $bookingResults, $reservationsResults);
+    
+    //         // Suggest similar term if no results found
+    //         if (empty($this->searchResults)) {
+    //             $this->suggestion = $this->findSimilarTerm($this->searchQuery);
+    //         }
+    
+    //     } catch (\Exception $e) {
+    //         $this->searchResults = [];
+    //         $this->searchError = 'Unable to perform search';
+    //         \Log::error("Search Error: {$e->getMessage()}");
+    //     }
+    // }
     
     
     
