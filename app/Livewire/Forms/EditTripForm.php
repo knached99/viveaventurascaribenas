9d484fb620c33d845ac929bb8eed05bbaa3b726e
knached99 <khalednached@gmail.com>
2025-03-24 15:23:57 -0400

    Reverting changes

--- app/Livewire/Forms/EditTripForm.php
+++ app/Livewire/Forms/EditTripForm.php
@@ -336,8 +336,11 @@ class EditTripForm extends Component
         }
 
 
-        public function rules(){
-        // Validate input data
+ 
+       
+        public function editTrip(): void
+        {
+            // Validate input data
             $rules = [
                 'tripLocation' => 'required|string|max:255',
                 'tripLandscape' => 'required|array',
@@ -357,29 +360,11 @@ class EditTripForm extends Component
             }
         
             if (empty($this->trip->tripPhoto)) {
-                $rules['tripPhotos.*'] = 'array|max:6';
-                $rules['tripPhotos.*'] = 'image|mimes:jpeg,png,jpg|max:2048';
+                $rules['tripPhotos'] = 'array|max:6';
+                $rules['tripPhotos'] = 'image|mimes:jpeg,png,jpg|max:2048';
             }
-        }
         
-        protected $messages = [
-            'tripLocation.required' => 'Trip location is required',
-            'tripLocation.max'=> 'Trip location name cannot exceed 255 characters',
-            'tripLandscape.required'=>'Trip landscape(s) are required',
-            'tripAvailability.required'=>'Trip availability is required',
-            'tripDescription.required'=>'Trip description is required',
-            'tripActivities.required'=>'Trip activities are required',
-            'num_trips.required'=>'Please enter a number for the number of slots you want to make available for this trip',
-
-        ];
-       
-        public function editTrip(): void
-        {
-            
-
-            
-        
-            $this->validate();
+            $this->validate($rules);
         
             try {
                 $tripModel = TripsModel::findOrFail($this->trip->tripID);
