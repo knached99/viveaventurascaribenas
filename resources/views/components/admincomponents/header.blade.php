e90a8493e944786ef799fbf7e4ca8827f920cbae
knached99 <khalednached@gmail.com>
2025-03-26 00:15:35 -0400

    Test defer ChartJS

--- resources/views/components/admincomponents/header.blade.php
+++ resources/views/components/admincomponents/header.blade.php
@@ -83,7 +83,7 @@
     {{-- <script src="https://cdn.ckeditor.com/ckeditor5/35.0.1/classic/ckeditor.js"></script> --}}
 
     <!-- Loading ChartJS Open Source Library -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js"
+    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js"
         integrity="sha512-L0Shl7nXXzIlBSUUPpxrokqq4ojqgZFQczTYlGjzONGTDAcLremjwaWv5A+EDLnxhQzY5xUZPWLOLqYRkY0Cbw=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     @livewireStyles
