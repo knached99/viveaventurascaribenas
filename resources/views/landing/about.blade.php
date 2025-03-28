4c0746a2089bc49a6c102af24239a2a620640d29
knached99 <khalednached@gmail.com>
2025-03-28 02:24:01 -0400

    Replacing image

--- resources/views/landing/about.blade.php
+++ resources/views/landing/about.blade.php
@@ -2,7 +2,7 @@
 <x-travelcomponents.navbar />
 
 <section class="hero-wrap hero-wrap-2 js-fullheight"
-    style="background-image: url({{ asset('assets/images/campeche_mexico_3.jpg') }});" data-stellar-background-ratio="0.5">
+    style="background-image: url({{ asset('assets/images/campeche_mexico_4.jpg') }});" data-stellar-background-ratio="0.5">
     <div class="overlay"></div>
     <div class="container">
         <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-center">
