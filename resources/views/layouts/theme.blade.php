d7a5573687f04fe08c97d4d57edc14df66bf490a
knached99 <khalednached@gmail.com>
2025-03-28 02:42:03 -0400

    Adding bg image to login page

--- resources/views/layouts/theme.blade.php
+++ resources/views/layouts/theme.blade.php
@@ -1,6 +1,6 @@
 
     <x-admincomponents.header/>
-
+    <body style="background-image: url({{ asset('assets/images/cancun_mexico_2.jpg') }}); background-size: cover; background-repeat: no-repeat; background-position: center;">
 
         <div>
         
