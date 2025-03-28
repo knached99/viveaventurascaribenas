b762c9bac79eb0a6946fb9f420ace3ede224e39c
knached99 <khalednached@gmail.com>
2025-03-28 02:20:27 -0400

    Changing bg images on about and destinations pages

--- resources/views/landing/destinations.blade.php
+++ resources/views/landing/destinations.blade.php
@@ -2,7 +2,7 @@
 
 <x-travelcomponents.navbar />
 
-<section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url({{ asset('assets/images/bg_1.jpg') }});"
+<section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url({{ asset('assets/images/campeche_mexico_2.jpg') }});"
     data-stellar-background-ratio="0.5">
     <div class="overlay"></div>
     <div class="container">
