a1eaa111172a6e3e0e3701895bc567c9b7f25fa3
knached99 <khalednached@gmail.com>
2025-05-15 16:30:35 -0400

    Added logging in maxmind service

--- app/Services/MaxMindService.php
+++ app/Services/MaxMindService.php
@@ -53,6 +53,7 @@ class MaxMindService
                 'longitude' => $record->location->longitude ?? null,
             ];
 
+            \Log::info('Location data: '.json_encode($location));
             // Cache the location for 1 day
             Cache::put($cacheKey, $location, 1440);
 
